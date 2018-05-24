4992f9e7badcb2ce361c226ee2d0bbb99e26f7e8
Noriaki Nakano <nnakano@ucsc.edu>
2018-05-24 10:52:46 -0700

    added legend plus data

--- assignment6/japanmap.js
+++ assignment6/japanmap.js
@@ -31,7 +31,10 @@ var japanese_key = {
 	"number" : "番号", 
 	"prefecture": "都道府県",
 	"density": "密度",
-	"population_density": "人口密度" 
+	"populationdensity": "人口密度", 
+	"population":"合計",
+	"male":"男",
+	"female":"女"
 };
 
 //Id conversion of geoJSON for Japan 
@@ -51,93 +54,134 @@ var color_scale = d3.scaleLinear()
 	
 var data; // store data from CSV file 
 
-// load CSV file 
-d3.csv("japan_populationdensity.csv", (e, d) => {
-	
-	data = d;
-	
-	// set up min, pivot, and max
-	var min =  d3.min(d.map((v) => {
-		return parseFloat(v[japanese_key["population_density"]]);
-	}));
-	
-	var mean = d3.mean(d.map((v) => {
-		return parseFloat(v[japanese_key["population_density"]]);
-	}));
-	
-	var max = d3.max(d.map((v) => {
-		return parseFloat(v[japanese_key["population_density"]]);
-	}));
-	
-	// set color scale domain 
-	color_scale.domain([
-		min, 
-		mean,
-		max	
-	]);
-	
-	// draw legend colored rectangles
-	svg.append("rect")
-		.attr("x", 0)
-		.attr("y", 0)
-		.attr("width",380)
-		.attr("height", 180)
-		.attr("fill", "lightgrey")
-		.style("stroke-size", "1px");
+draw("populationdensity"); // initial call
 
-	svg.append("circle")
-		.attr("r", 20)
-		.attr("cx", 30)
-		.attr("cy", 30)
-		.style("fill", color_scale(max));
+function draw(type)
+{// get CSV data and drwa geoJSON
+	svg.selectAll("*").remove();
+	d3.csv("japan_" + type + ".csv", (e, d) => {
 	
-	svg.append("circle")
-		.attr("r", 20)
-		.attr("cx", 30)
-		.attr("cy", 90)
-		.style("fill", color_scale(mean));
-
-	svg.append("circle")
-		.attr("r", 20)
-		.attr("cx", 30)
-		.attr("cy", 150)
-		.style("fill", color_scale(min));
-
-	svg.append("text")
-		.attr("class", "label")
-		.attr("x", 370)
-		.attr("y", 35)
-		.style("text-anchor", "end")
-		.text("maximum: " + max.toFixed(1) + " people/kilometer^2");
-
-	svg.append("text")
-		.attr("class", "label")
-		.attr("x", 360)
-		.attr("y", 95)
-		.style("text-anchor", "end")
-		.text("mean: " + mean.toFixed(1) + " people/kilometer^2");
-
-	svg.append("text")
-		.attr("class", "label")
-		.attr("x", 350)
-		.attr("y", 155)
-		.style("text-anchor", "end")
-		.text("minimum: " + min.toFixed(1) + " people/kilometer^2");
-	
-});
-	
-d3.json("japan.json", (e, d) => {// load JSON file 
-	
-	// draw GeoJSON 
-	svg.selectAll("path")
-		.data(d.features)
-		.enter()
-		.append("path")
-		.attr("d", (v)=> {return path(v);}) // send path value to append 
-		.attr("fill", (v, i) => color_scale(data[id_conversion[i] - 1][japanese_key["population_density"]]))
-		.attr("stroke", "#222");
+		data = d;
 		
-});
-
-
+		console.log(d);
+		
+		// set up min, pivot, and max
+		var min =  d3.min(d.map((v) => {
+			return parseFloat(v[japanese_key[type]]);
+		}));
+		
+		var mean = d3.mean(d.map((v) => {
+			return parseFloat(v[japanese_key[type]]);
+		}));
+		
+		var max = d3.max(d.map((v) => {
+			return parseFloat(v[japanese_key[type]]);
+		}));
+		
+		// set color scale domain 
+		color_scale.domain([
+			min, 
+			mean,
+			max	
+		]);
+		
+		// init legend 
+		var legend = svg.append("defs")
+			.append("svg:linearGradient")
+			.attr("id", "gradient")
+			.attr("x1", "100%")
+			.attr("y1", "100%")
+			.attr("x2", "100%")
+			.attr("y2", "0%")
+			.attr("spreadMethod", "pad");
+		
+		legend.append("stop")
+			.attr("offset", "0%")
+			.attr("stop-color", "#00F")
+			.attr("stop-opacity", 1);
 	
+		legend.append("stop")
+			.attr("offset", "50%")
+			.attr("stop-color", "#0F0")
+			.attr("stop-opacity", 1);
+
+		legend.append("stop")
+			.attr("offset", "100%")
+			.attr("stop-color", "#F00")
+			.attr("stop-opacity", 1);
+		
+		svg.append("rect")
+			.attr("x", -10)
+			.attr("y", -10)
+			.attr("width",400)
+			.attr("height", 200)
+			.attr("fill", "lightgrey")
+			.style("stroke-size", "1px");
+			
+		/*
+		svg.append("circle")
+			.attr("r", 20)
+			.attr("cx", 30)
+			.attr("cy", 30)
+			.style("fill", color_scale(max));
+		
+		svg.append("circle")
+			.attr("r", 20)
+			.attr("cx", 30)
+			.attr("cy", 90)
+			.style("fill", color_scale(mean));
+
+		svg.append("circle")
+			.attr("r", 20)
+			.attr("cx", 30)
+			.attr("cy", 150)
+			.style("fill", color_scale(min));
+		*/
+		//draw legend text
+		svg.append("text")
+			.attr("class", "label")
+			.attr("x", 80)
+			.attr("y", 13)
+			.style("text-anchor", "start")
+			.text("maximum: " + max.toFixed(1) + (type == "populationdensity" ? " people/kilometer^2" : " people"));
+
+		svg.append("text")
+			.attr("class", "label")
+			.attr("x", 80)
+			.attr("y", 95)
+			.style("text-anchor", "start")
+			.text("mean: " + mean.toFixed(1) + (type == "populationdensity" ? " people/kilometer^2" : " people"));
+
+		svg.append("text")
+			.attr("class", "label")
+			.attr("x", 80)
+			.attr("y", 175)
+			.style("text-anchor", "start")
+			.text("minimum: " + min.toFixed(1) + (type == "populationdensity" ? " people/kilometer^2" : " people"));
+		
+		//draw gradient 
+		svg.append("rect")
+			.attr("width", 50)
+			.attr("height", 180)
+			.style("fill", "url(#gradient)")
+			.attr("transform", "translate(0,0)");
+		
+	});
+	d3.json("japan.json", (e, d) => {// load JSON file 
+	
+		// draw GeoJSON 
+		svg.selectAll("path")
+			.data(d.features)
+			.enter()
+			.append("path")
+			.attr("d", (v)=> {return path(v);}) // send path value to append 
+			.attr("fill", (v, i) => color_scale(data[id_conversion[i] - 1][japanese_key[type]]))
+			.attr("stroke", "#222");
+			
+	});
+}// End draw	
+
+function change_data()
+{// called when the selection gets changed 
+	draw(document.getElementById("data_select").value);
+}// End change_data
